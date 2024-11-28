3dd9b358231c092a3a367effed83558d5093bbe7
duongminh98 <duongtuanminh2005@gmail.com>
2024-11-28 22:56:24 +0700

    Revert "Recommendation and Review"
    This reverts commit 0dbbd95e

---
+++ src/main/java/Library/backend/Session/SessionManager.java
@@ -0,0 +1,25 @@
+package Library.backend.Session;
+
+import Library.backend.Login.Model.Member;
+
+public class SessionManager {
+    private static SessionManager instance;
+    private Member loggedInMember;
+
+    private SessionManager() {}
+
+    public static SessionManager getInstance() {
+        if (instance == null) {
+            instance = new SessionManager();
+        }
+        return instance;
+    }
+
+    public Member getLoggedInMember() {
+        return loggedInMember;
+    }
+
+    public void setLoggedInMember(Member loggedInMember) {
+        this.loggedInMember = loggedInMember;
+    }
+}
