c7f7a45edf7300605c6f9868b0c94ed55088c9e2
Khuat Tuan Anh <ppmelon2004@gmail.com>
2024-11-30 10:04:54 +0700

    Add Notification

---
+++ src/main/java/Library/ui/Notification/Notification.java
@@ -0,0 +1,60 @@
+package Library.ui.Notification;
+
+import javafx.animation.PauseTransition;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+public class Notification {
+    protected Text Message;
+
+    protected Stage MessageStage;
+
+    protected StackPane root;
+
+    public Notification(String message) {
+        Message = new Text(message);
+
+        Message.setFont(Font.font("Barlow Medium", 14));
+        Message.setWrappingWidth(150);
+        Message.setTextAlignment(TextAlignment.LEFT);
+        Message.setFill(Color.rgb(25,25,25));
+
+        MessageStage = new Stage();
+        MessageStage.initStyle(StageStyle.TRANSPARENT);
+        MessageStage.setResizable(false);
+
+        root = new StackPane(Message);
+        root.setStyle("-fx-background-radius: 5px; "
+                + "-fx-border-radius: 5px; "
+                + "-fx-border-width: 2px; "
+                + "-fx-border-color: rgb(72,72,72);"
+                + "-fx-background-color: rgb(255,255,255);");
+        root.setPrefWidth(200);
+        root.setPrefHeight(80);
+
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+
+        MessageStage.setScene(scene);
+
+        MessageStage.setX(155);
+        MessageStage.setY(650);
+
+    }
+
+    public void display() {
+        MessageStage.show();
+
+        // Close the notification after 4 seconds
+        PauseTransition delay = new PauseTransition(Duration.seconds(4));
+        delay.setOnFinished( event -> MessageStage.close() );
+        delay.play();
+    }
+}
