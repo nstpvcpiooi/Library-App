5a821e2b3fb362a2555e0c09f7dab3e91b23a2e8
Khuat Tuan Anh <ppmelon2004@gmail.com>
2024-11-30 21:00:14 +0700

    User Manage

--- src/main/java/Library/ui/Admin/UserManageController.java
+++ src/main/java/Library/ui/Admin/UserManageController.java
@@ -1,13 +1,17 @@
 package Library.ui.Admin;
 
+import Library.backend.Login.DAO.MemberDAOImpl;
 import Library.backend.Login.Model.Member;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class UserManageController implements Initializable {
@@ -46,5 +50,13 @@ public class UserManageController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        List<Member> members = MemberDAOImpl.getInstance().DisplayMembers();
+        MemberList = FXCollections.observableArrayList(members);
+        UserName.setCellValueFactory(new PropertyValueFactory<Member, String>("userName"));
+        Password.setCellValueFactory(new PropertyValueFactory<Member, String>("password"));
+        Email.setCellValueFactory(new PropertyValueFactory<Member, String>("email"));
+        Phone.setCellValueFactory(new PropertyValueFactory<Member, String>("phone"));
+        Duty.setCellValueFactory(new PropertyValueFactory<Member, Integer>("duty"));
+        table.setItems(MemberList);
     }
 }
